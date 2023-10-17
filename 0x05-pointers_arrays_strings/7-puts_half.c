9f8d6390c66ecd3388d30834056fdac14e7745f9
slowlybutsteadylearning <siribro93@gmail.com>
2023-10-16 17:42:13 -0700

    initial commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,29 +23,4 @@ void puts_half(char *str)
 		_putchar(str[z]);
 	}
 	_putchar(10);
-#include "main.h"
-#include <string.h>
-/**
- * puts_half - Entry point
- * function that prints half of a string
- * @str: parameter of a pointer
- */
-void puts_half(char *str)
-{
-	int x, y, z;
-
-	x = strlen(str);
-	if (x / 2 == 1)
-	{
-		y = x / 2 + 1;
-	}
-	else
-	{
-		y = x / 2;
-	}
-	for (z = y; z < x; z++)
-	{
-		_putchar(str[z]);
-	}
-	_putchar(10);
-}}
+}
