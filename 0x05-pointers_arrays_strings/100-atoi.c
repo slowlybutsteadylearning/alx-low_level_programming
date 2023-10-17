d463e3cffa4e6027f308c18b37927c743e907300
slowlybutsteadylearning <siribro93@gmail.com>
2023-10-16 17:48:12 -0700

    initial commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,35 @@
 #include "main.h"
 
 /**
- * _atoi - converts string to integer
- * @s: string to convert
- *
- * Return: returns integer value
+ * _atoi - Entry point
+ * function that converts a string to an integer
+ * @s: the pointer parameter to convert
+ * Return: on Success - an integer
  */
 int _atoi(char *s)
 {
-	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
+	int a;
+	unsigned int nah = 0;
+	int min = 1;
+	int sec = 0;
 
-	for (i = 0; i < _strlen(s); i++)
+	while (s[a])
 	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
-		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[a] == 45)
 		{
-			c++;
+			min *= 1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			sec = 1;
+			nah = (nah * 10) + (s[a] - '0');
+			a++;
+		}
+		if (sec == 1)
+		{
+			break;
 		}
 	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	} else
-	{
-		num *= -1;
-	}
-	return (num);
-}
+	nah *= min;
+	return (nah);
+}}
