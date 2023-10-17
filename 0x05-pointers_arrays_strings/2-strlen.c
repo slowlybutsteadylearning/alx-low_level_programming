fb76c45ce2211d54d02365aae55189156827e225
slowlybutsteadylearning <siribro93@gmail.com>
2023-10-16 17:20:54 -0700

    initial commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,17 @@
-#include "main"
-#include <string.h>
+#include "main.h"
 
 /**
- * _strlen - Entry point
- * function that returns the length of a string
- * @s: string pointer
- * Return: length of a string
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
  */
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
 }
 
